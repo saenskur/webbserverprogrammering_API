a8b2afc753a473d86a18ae3b204a7e0f1c2228fd
saenskur <saenskur2@gmail.com>
2022-01-14 08:59:52 +0100

    commition

--- uppgift3/HelloWorld/register/forms.py
+++ uppgift3/HelloWorld/register/forms.py
@@ -10,6 +10,3 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'testclass'}),
-        }
