931b799fedd6492b5db7eeb2ae3fd11d8ceb0fa3
saenskur <70852221+saenskur@users.noreply.github.com>
2022-05-30 13:51:06 +0200

    Delete uppgift3/HelloWorld directory

--- uppgift3/HelloWorld/tasklist/urls.py
+++
@@ -1,9 +0,0 @@
-#from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include 
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="list"),
-    path('add/', views.add, name="add"),
-]
