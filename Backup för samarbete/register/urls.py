8aba2ca6ce87c162841e94efbef3c4468dcdcf46
saenskur <70852221+saenskur@users.noreply.github.com>
2022-05-30 13:50:44 +0200

    Delete Backup för samarbete/register directory

--- Backup för samarbete/register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-#Person 3, här behövs importeras views från denna mapp
-
-urlpatterns = [
-    #((Person 2) här behövs läggas till en koppling till view-funktionen register)
-    path('',  , name="register"),
-
-]
